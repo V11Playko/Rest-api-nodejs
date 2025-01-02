837b121d519a72d2f9a3ef90a3dea96ea0ed2e69
Heinner Vega <heinnervega20@gmail.com>
2025-01-01 20:27:26 -0500

    Add health method

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,3 +1,4 @@
+import routes from '@routes/routes';
 import express, { Application } from 'express'
 import morgan from 'morgan'
 
@@ -5,5 +6,7 @@ const app: Application = express();
 
 app.use(express.json())
 app.use(morgan("dev"));
+
+app.use("/api/v1", routes());
 
 export default app;
