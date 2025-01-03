e9f38654e655bf84368fad7df74b522a649d706f
Heinner Vega <heinnervega20@gmail.com>
2025-01-03 11:56:10 -0500

    Add CRUD of Roles

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,19 +1,25 @@
 import { IUserRepository, IUserService, User } from 'types/UsersTypes';
+import { IRolesRepository, IRolesService, Roles } from 'types/RolesTypes';
 import { Router } from 'express'
 import { UserService } from '@service/UserService';
 import { UserRepository } from '@repository/UserRepository';
+import { RolesService } from '@service/RolesService';
+import { RolesRepository } from '@repository/rolesRepositories';
 
 const router = Router();
 
 const userRepository: IUserRepository = new UserRepository()
 const userService: IUserService = new UserService(userRepository);
 
+const rolesRepository: IRolesRepository = new RolesRepository()
+const rolesService: IRolesService = new RolesService(rolesRepository);
+
 export default () => {
     router.get('/health', (req, res) => {
         res.send("Api es Healthy!!");
     })
 
- //Get
+ // User Routes
  router.get("/users", async (req, res) => {
     const users = await userService.findUsers();
     res.json(users);
@@ -40,5 +46,36 @@ export default () => {
     const users = await userService.deleteUser(req.params.id);
     res.json(users);
   });
+
+ // Roles Routes
+ //Get
+ router.get("/roles", async (req, res) => {
+  const roles = await rolesService.findRoles();
+  res.json(roles);
+});
+
+router.get("/roles/:id", async (req, res) => {
+  const roles = await rolesService.findRolesById(req.params.id);
+  res.json(roles);
+});
+
+//Create
+router.post("/roles", async (req, res) => {
+  const newRole: Roles = req.body;
+  const result = await rolesService.createRoles(newRole);
+
+  res.json(result);
+});
+
+router.put("/roles/:id", async (req, res) => {
+  const users = await rolesService.updateRoles(req.params.id, req.body);
+  res.json(users);
+});
+router.delete("/roles/:id", async (req, res) => {
+  const roles = await rolesService.deleteRoles(req.params.id);
+  res.json(roles);
+});
+
+
   return router;
 };
