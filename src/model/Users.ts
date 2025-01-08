1dbfaa2e5a1995831266f5497a620cd1d8d676ba
Heinner Vega <heinnervega20@gmail.com>
2025-01-08 10:19:03 -0500

    Add encrypt password with Bcrypt

--- src/model/Users.ts
+++ src/model/Users.ts
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 import { User } from "types/UsersTypes";
+import bcrypt from "bcrypt";
 
 const UserSchema: Schema = new Schema<User>(
   {
@@ -29,4 +30,22 @@ const UserSchema: Schema = new Schema<User>(
   }
 );
 
+UserSchema.pre<User>("save", async function (next) {
+  if (this.isModified("password") || this.isNew) {
+    const salt = await bcrypt.genSalt(12);
+    const hash = await bcrypt.hash(this.password, salt);
+    this.password = hash;
+  }
+  next();
+});
+
+UserSchema.method("comparePassword", async function (password: string): Promise<boolean> {
+  return await bcrypt.compare(password, this.password as string);
+});
+
+UserSchema.methods.toJSON = function () {
+  const userObj = this.toObject();
+  delete userObj.password;
+  return userObj;
+};
 export const UserModel = mongoose.model<User>("User", UserSchema);
--- src/types/UsersTypes.ts
+++ src/types/UsersTypes.ts
@@ -1,3 +1,4 @@
+import { Document } from "mongoose";
 import { Query, Repository } from "./RepositoryTypes";
 
 export interface User extends Document {
@@ -5,6 +6,7 @@ export interface User extends Document {
   username: string;
   email: string;
   password: string;
+  comparePassword(password: string): Promise<boolean>;
 }
 
 export interface IUserRepository extends Repository<User> {
@@ -14,8 +16,8 @@ export interface IUserRepository extends Repository<User> {
 export interface IUserService {
   createUser(user: User): Promise<User>;
   findUsers(): Promise<User[]>;
-  findUsersById(id: string): Promise<User | null>
-  findUsersByEmail(email: string): Promise<User | null>
-  updateUser(id: string, user: Partial<User>): Promise<User | null>
-  deleteUser(id: string): Promise<boolean>
+  findUsersById(id: string): Promise<User | null>;
+  findUsersByEmail(email: string): Promise<User | null>;
+  updateUser(id: string, user: Partial<User>): Promise<User | null>;
+  deleteUser(id: string): Promise<boolean>;
 }
