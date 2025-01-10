949cf40a698a619eadaa8053ef3e6008a00300ef
Heinner Vega <heinnervega20@gmail.com>
2025-01-10 11:18:31 -0500

    Add JWT in login

--- src/auth/authControllers.ts
+++ src/auth/authControllers.ts
@@ -2,6 +2,7 @@ import { UserRepository } from "@repository/UserRepository";
 import { UserService } from "@service/UserService";
 import { json, Request, Response } from "express";
 import { IUserRepository, IUserService, User } from "types/UsersTypes";
+import jwt from "jsonwebtoken";
 
 const userRepository: IUserRepository = new UserRepository();
 const userService: IUserService = new UserService(userRepository);
@@ -26,7 +27,8 @@ export const loginUser = async (req: Request, res: Response) => {
     if (!user) return res.status(400).json({ message: "Invalid user or password" });
     const comparePass = await user.comparePassword(password);
     if (!comparePass) return res.status(400).json({ message: "Invalid user or password" });
-    res.json(user);
+    const token = jwt.sign({ id: user._id, email: user.email, username: user.username }, "ClaveSecreta", { expiresIn: "1h" });
+    res.json(token);
   } catch (error) {
     console.log("error :>> ", error);
     res.status(500).json(error);
