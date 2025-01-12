4f13aecb3964e54424bcfe3da7f703b31a8b8d54
Heinner Vega <heinnervega20@gmail.com>
2025-01-12 18:54:33 -0500

    Updated routes and add posts.http

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { createUser, deleteUser, findUsers, findUsersById, updateUser } from '@controller/usersControllers';
 import { createRoles, deleteRoles, findRoles, findRolesById, updateRoles } from '@controller/rolesControllers';
+import { createPosts, deletePosts, findPosts, findPostsById, updatePosts } from "@controller/postsControllers";
 import { loginUser, registerUser } from '@auth/authControllers';
 
 const router = Router();
@@ -33,6 +34,13 @@ export default () => {
   router.put("/roles/:id", updateRoles);
 
   router.delete("/roles/:id", deleteRoles);
+
+  // Posts Routes
+    router.get("/posts", findPosts);
+    router.get("/posts/:id", findPostsById);
+    router.post("/posts", createPosts);
+    router.put("/posts/:id", updatePosts);
+    router
 
   return router;
 };
